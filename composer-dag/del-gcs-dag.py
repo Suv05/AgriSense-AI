0c5e19fc52b714b8a59c45cfa4b67967548aba3b
suvam <suvampravu@gmail.com>
2025-08-08 12:36:51 +0530

    commit changes

---
+++ composer-dag/del-gcs-dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.gcs import GCSDeleteObjectsOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="daily_gcs_cleanup",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="30 5 * * *",  # Runs daily at 11:00 AM IST (5:30 UTC)
+    catchup=False,
+    tags=['gcs', 'cleanup'],
+) as dag:
+
+    delete_earth_engine_data = GCSDeleteObjectsOperator(
+        task_id="delete_earth_engine_data",
+        bucket_name="careful-trainer-p1-agri-datalake",
+        prefix="raw/earth-engine/",
+        objects=None,
+    )
+
+    delete_weather_data = GCSDeleteObjectsOperator(
+        task_id="delete_weather_data",
+        bucket_name="careful-trainer-p1-agri-datalake",
+        prefix="raw/weather/",
+        objects=None,
+    )
+
+    delete_earth_engine_data >> delete_weather_data
